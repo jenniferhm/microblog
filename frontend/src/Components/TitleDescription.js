ac0bf4fabc75ecfbf13f934508d6cdf79585bfad
Jennifer Ma <jenniferma93@gmail.com>
2019-10-03 16:30:20 -0700

    added thumbs icons

--- frontend/src/Components/TitleDescription.js
+++ frontend/src/Components/TitleDescription.js
@@ -6,6 +6,7 @@ class TitleDescription extends Component {
       <div>
         <h3>{this.props.title.title}</h3>
         <p>{this.props.title.description}</p>
+        <p>0 votes. <span><i class="fas fa-thumbs-up"></i></span> <span><i class="fas fa-thumbs-down"></i></span></p>
       </div>
     );
   }
